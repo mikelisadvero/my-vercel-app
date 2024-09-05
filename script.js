55392ead52f851b9444965f00ca0cafaf2706f05
mikelisadvero <mikelis@advero.lv>
2024-09-05 14:03:27 +0300

    Updated everything

--- script.js
+++ script.js
@@ -5,30 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const youtubeScraper = document.getElementById('youtubeScraper');
     const googleSitesInput = document.querySelector('#googleScraper .input-field:last-child');
 
-    // Initial check to set the correct background and visibility on load
-    if (toggle.checked) {
-        googleScraper.style.display = 'none';
-        youtubeScraper.style.display = 'block';
-        container.className = 'container youtube-bg';
-        googleSitesInput.style.display = 'none'; // Hide Google-specific input
-    } else {
-        googleScraper.style.display = 'block';
-        youtubeScraper.style.display = 'none';
-        container.className = 'container google-bg';
-        googleSitesInput.style.display = 'block'; // Show Google-specific input
-    }
-
-    toggle.addEventListener('change', function() {
-        if (this.checked) {
+    function updateUI() {
+        if (toggle.checked) {
             googleScraper.style.display = 'none';
             youtubeScraper.style.display = 'block';
             container.className = 'container youtube-bg';
-            googleSitesInput.style.display = 'none'; // Hide Google-specific input
+            googleSitesInput.style.display = 'none';
         } else {
             googleScraper.style.display = 'block';
             youtubeScraper.style.display = 'none';
             container.className = 'container google-bg';
-            googleSitesInput.style.display = 'block'; // Show Google-specific input
+            googleSitesInput.style.display = 'block';
         }
-    });
+    }
+
+    toggle.addEventListener('change', updateUI);
+    updateUI(); // Ensure UI is correctly set on initial load
 });
