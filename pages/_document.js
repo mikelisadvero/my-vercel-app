d23969ff344d7b8688e5b47053a6c814601715c8
mikelisadvero <mikelis@advero.lv>
2024-09-06 10:26:14 +0300

    misc

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    {/* You can add custom tags here, such as stylesheets or fonts */}
+                    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
